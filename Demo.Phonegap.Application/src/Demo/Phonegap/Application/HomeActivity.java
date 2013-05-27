25f5d8d587250c5ccddff8fa278a6dd202b1b0f3
Christopher Weiss <info@cweiss.de>
2013-05-27 15:40:16 +0200

    init

---
+++ Demo.Phonegap.Application/src/Demo/Phonegap/Application/HomeActivity.java
@@ -0,0 +1,15 @@
+package Demo.Phonegap.Application;
+
+import android.os.Bundle;
+import org.apache.cordova.DroidGap;
+
+public class HomeActivity extends DroidGap {
+  /**
+   * Called when the activity is first created.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    super.loadUrl("file:///android_asset/www/index.html");
+  }
+}
